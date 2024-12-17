38bbb9646bfb2063dc70153d2b127b5f3da75955
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-12-18 11:45:06 +1300

    transforms

--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 import os
 import re
+from collections.abc import Sequence
 from dataclasses import dataclass
+
 PRIMARY_FILE = True
 
 
@@ -22,13 +24,13 @@ class Component:
 def xml_encode(s: str) -> str:
     return s.replace(">", "&gt;").replace("<", "&lt;")
 
-def get_xml_type(ty: str) -> list[tuple[str, str]]:
+def get_xml_type(ty: str) -> list[tuple[str, str]] | str:
     lens = "LensValue"
     unsigned = "unsigned"
     if ty[: len(lens)] == lens:
         return get_xml_type(ty[len(lens) + 1 : -1])
     if ty == "float" or ty == "double":
-        return [("", "xs:float")]
+        return [("", "xs:decimal")]
     if ty[:3] == "int":
         return [("", "xs:int")]
     if ty[: len(unsigned)] == unsigned or ty[:4] == "uint":
@@ -39,8 +41,8 @@ def get_xml_type(ty: str) -> list[tuple[str, str]]:
         return [("", "NoitaBool")]
     if ty == "vec2":
         return [
-            (".x", "xs:float"),
-            (".y", "xs:float"),
+            (".x", "xs:decimal"),
+            (".y", "xs:decimal"),
         ]
     if ty == "ivec2":
         return [
@@ -49,15 +51,15 @@ def get_xml_type(ty: str) -> list[tuple[str, str]]:
         ]
     if ty == "types::fcolor":
         return [
-            (".r", "xs:float"),
-            (".g", "xs:float"),
-            (".b", "xs:float"),
-            (".a", "xs:float"),
+            (".r", "xs:decimal"),
+            (".g", "xs:decimal"),
+            (".b", "xs:decimal"),
+            (".a", "xs:decimal"),
         ]
     if ty == "ValueRange":
         return [
-            (".min", "xs:float"),
-            (".max", "xs:float"),
+            (".min", "xs:decimal"),
+            (".max", "xs:decimal"),
         ]
     if ty == "ValueRangeInt":
         return [
@@ -66,10 +68,10 @@ def get_xml_type(ty: str) -> list[tuple[str, str]]:
         ]
     if ty == "types::aabb":
         return [
-            (".min_x", "xs:float"),
-            (".min_y", "xs:float"),
-            (".max_x", "xs:float"),
-            (".max_y", "xs:float"),
+            (".min_x", "xs:decimal"),
+            (".min_y", "xs:decimal"),
+            (".max_x", "xs:decimal"),
+            (".max_y", "xs:decimal"),
         ]
     if ty == "types::iaabb":
         return [
@@ -78,6 +80,9 @@ def get_xml_type(ty: str) -> list[tuple[str, str]]:
             (".max_x", "xs:int"),
             (".max_y", "xs:int"),
         ]
+    # objects
+    if ty == "types::xform":
+        return "Transform"
     # defined to be invalid
     if ty in ["EntityID", "WormPartPositions"]:
         return []
@@ -106,21 +111,27 @@ def render_sub_field(field: Field, suffix: str, docs: str, ty: str) -> str:
     return f"""\t\t\t<xs:attribute name="{field.name}{suffix}" type="{ty}" default="{field.default if field.default != "-" else ("" if ty == "xs:string" else "0")}" />"""
 
 
-def render_field(field: Field) -> str:
+def render_field(field: Field) -> tuple[str, str]:
     tys = get_xml_type(field.ty)
+    if type(tys) is str:
+        return f"""\t\t\t<xs:sequence minOccurs="0"> <xs:element name="{field.name}" type="{tys}" /></xs:sequence>""", ""
     if len(tys) == 0:
-        return "<!-- Some Object -->"
+        return "", f"\t\t\t<!-- Some Unknown Type: {field.ty} for {field.name} -->"
     docs = ""
     if field.default != "-":
         docs = f"`{field.default}` - `{field.values}`"
-    return "\n".join([render_sub_field(field, suffix, docs, ty) for suffix, ty in tys])
+    return "", "\n".join([render_sub_field(field, suffix, docs, ty) for suffix, ty in tys])
 
 
 def render_component(comp: Component) -> str:
+    fields =[render_field(x) for x in comp.fields] 
+    attrs = [x[1] for x in fields if x[1] != ""]
+    objects = [x[0] for x in fields if x[0] != ""]
     return f"""
 	<xs:element name="{comp.name}">
 		<xs:complexType>
-{"\n".join([render_field(x) for x in comp.fields])}
+{"\n".join(objects)}
+{"\n".join(attrs)}
 		</xs:complexType>
 	</xs:element>"""[
         1:
@@ -207,13 +218,21 @@ out = f"""
 			<xs:enumeration value="1" />
 		</xs:restriction>
 	</xs:simpleType>
+	<xs:complexType name="Transform">
+		<xs:attribute name="position.x" type="xs:decimal" default="0" />
+		<xs:attribute name="position.y" type="xs:decimal" default="0" />
+		<xs:attribute name="scale.x" type="xs:decimal" default="0" />
+		<xs:attribute name="scale.y" type="xs:decimal" default="0" />
+		<xs:attribute name="rotation" type="xs:decimal" default="0" />
+	</xs:complexType>
 	<xs:element name="Entity">
 		<xs:annotation>
 			<xs:documentation>Represents an entity that can be loaded into the world</xs:documentation>
 		</xs:annotation>
 		<xs:complexType>
 			<xs:sequence minOccurs="0">
-				<xs:choice maxOccurs="unbounded">
+				<xs:element name="Transform" type="Transform" />
+				<xs:choice maxOccurs="unbounded" minOccurs="0">
 					<xs:element ref="Entity" />
 					{"\n\t\t\t\t\t".join([f"<xs:element ref=\"{comp.name}\" />" for comp in components])}
 				</xs:choice>
